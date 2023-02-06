fe2fa8624f7fc1ec4b3b3b02256d3e8d593bd1a9
Arsh Suri <gunmala@gunmalas-MacBook-Pro.local>
2023-02-06 13:46:23 +0530

    get object from proposed db

--- authservice/data/data.go
+++ authservice/data/data.go
@@ -1,5 +1,42 @@
-package authservice
+package autservice
 
-func AddUserObject() {
+type user struct {
+	email        string
+	username     string
+	passwordhash string
+	fullname     string
+	createDate   string
+	role         int
+}
 
+func GetUserObject(email string) (user, bool) {
+	for _, user := range userList {
+		if user.email == email {
+			return user, true
+		}
+	}
+	return user{}, false
+}
+
+// checks if the password hash is valid
+func (u *user) ValidatePasswordHash(pswdhash string) bool {
+	return u.passwordhash == pswdhash
+}
+
+func AddUserObject(email string, username string, passwordhash string, fullname string, role int) bool {
+	newUser := user{
+		email:        email,
+		passwordhash: passwordhash,
+		username:     username,
+		fullname:     fullname,
+		role:         role,
+	}
+	// check if a user already exists
+	for _, ele := range userList {
+		if ele.email == email || ele.username == username {
+			return false
+		}
+	}
+	userList = append(userList, newUser)
+	return true
 }
